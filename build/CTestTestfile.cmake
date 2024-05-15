55e24ca794b592d6a07503049a2be4d798462a37
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-05-15 21:04:25 +1000

    Created a stub for operator!= in Account

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bromastone/Documents/cpp-projects/accounts-manager-cli
+# Build directory: /home/bromastone/Documents/cpp-projects/accounts-manager-cli/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/googletest-build")
---
+++ build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bromastone/Documents/cpp-projects/accounts-manager-cli/build/_deps/googletest-src
+# Build directory: /home/bromastone/Documents/cpp-projects/accounts-manager-cli/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
