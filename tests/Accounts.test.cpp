40329741ea934254c78766651cf13d5b14b3a658
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-05-16 18:15:54 +1000

    Written test for delete_account

--- tests/Accounts.test.cpp
+++ tests/Accounts.test.cpp
@@ -5,36 +5,93 @@
 #include "../include/Accounts.h"
 
 // Test the begin iterator
-
 TEST(AccountsBegin, EmptyAccounts) {
     Accounts accounts;
     std::vector<Account>::iterator it = accounts.begin();
     EXPECT_TRUE(it == accounts.end());
 }
 
+// Test the const iterator
 TEST(AccountsCBegin, EmptyAccounts) {
     Accounts accounts;
     std::vector<Account>::const_iterator it = accounts.cbegin();
     EXPECT_TRUE(it == accounts.cend());
 }
 
-// // Tests the creation and destruction of Accounts with getters
-// TEST(Constructor, NoAddedAccounts) {
-//     Accounts accounts;
-//     std::vector<Account> result = accounts.get_accounts();
-//     ASSERT_TRUE(result.empty());
-// }
+// Tests adding one account
+TEST(AccountsAddAccount, AddingOneAccount) {
+    Accounts accounts;
 
-// // Tests adding one account
-// TEST(AddAccount, AddingOneAccount) {
-//     Accounts accounts;
+    Account account(123, "DET login", { "Email: helloman@email.com", "Password: hellowoman" }, 929382);
+    accounts.add_account(account);
+    std::vector<Account>::const_iterator it = accounts.cbegin();
 
-//     Account account(123, "DET login", { "Email: helloman@email.com", "Password: hellowoman" }, 12345);
-//     accounts.add_account(account);
+    EXPECT_EQ(*it, account);
+}
 
-//     std::vector<Account> result_vector = accounts.get_accounts();
-//     std::cout << "RESULT SIZE: " << result_vector.size() << std::endl;
-// }
+// Tests adding multiple accounts
+TEST(AccountsAddAccount, AddingMultipleAccounts) {
+    Accounts accounts;
+    Account account_1(31293, "Bobby Smith", { "Username: bobsmith23", "Password: hungrybob02" }, 1234);
+    Account account_2(1984, "Stake", { "Email: Jordan Bellton", "Username: jbellton24", "Password: jbjbjbjb", "Password: Goodstuff" }, 4321);
+    Account account_3(123, "DET login", { "Email: helloman@email.com", "Password: hellowoman" }, 929382);
+    
+    std::vector<Account> expected;
+
+    expected.push_back(account_1);
+    expected.push_back(account_2);
+    expected.push_back(account_3);
+
+    accounts.add_account(account_1);
+    accounts.add_account(account_2);
+    accounts.add_account(account_3);
+
+    int i = 0;
+    for (auto it = accounts.cbegin(); it != accounts.cend(); it++) {
+        EXPECT_EQ(*it, expected.at(i));
+        i++;
+    }
+}
+
+// Delete Account
+TEST(AccountsDeleteAccount, DeletingOneAccount) {
+    Accounts accounts;
+
+    Account account(123, "DET login", { "Email: helloman@email.com", "Password: hellowoman" }, 929382);
+    accounts.add_account(account);
+    std::vector<Account>::iterator it = accounts.begin();
+    accounts.delete_account(it);
+    EXPECT_EQ(accounts.cbegin(), accounts.cend());
+}
+
+// Adds three accounts, deletes two of them, and sees if one remains
+TEST(AccountsDeleteAccount, DeletingMultipleAccounts) {
+    Accounts accounts;
+    Account account_1(31293, "Bobby Smith", { "Username: bobsmith23", "Password: hungrybob02" }, 1234);
+    Account account_2(1984, "Stake", { "Email: Jordan Bellton", "Username: jbellton24", "Password: jbjbjbjb", "Password: Goodstuff" }, 4321);
+    Account account_3(123, "DET login", { "Email: helloman@email.com", "Password: hellowoman" }, 929382);
+    
+    std::vector<Account> expected;
+
+    accounts.add_account(account_1);
+    accounts.add_account(account_2);
+    accounts.add_account(account_3);
+
+    auto it = accounts.begin();
+    accounts.delete_account(it);
+    accounts.delete_account(it);
+
+    EXPECT_EQ(*(it), account_3);
+}
+
+TEST(AccountsDeleteAccount, DeletingFromAnEmptyList) {
+    Accounts accounts;
+
+    std::vector<Account>::iterator it = accounts.begin();
+    
+    // EXPECT_EQ(accounts.cbegin(), accounts.cend());
+    EXPECT_THROW(accounts.delete_account(it), std::out_of_range);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
