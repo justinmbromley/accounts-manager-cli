1fcd7cb71b133c511d06d1d1f46736a77772c749
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-05-19 07:59:30 +1000

    Implemented the NewAccountIDGenerator class with the generate_new_account_id function

--- src/NewAccountIDGenerator.cpp
+++ src/NewAccountIDGenerator.cpp
@@ -6,5 +6,13 @@
 #include "../include/NewAccountIDGenerator.h"
 
 uint32_t NewAccountIDGenerator::generate_new_account_id(const Accounts& accounts) const {
-    return 31293;
+    AccountIDValidator account_id_validator;
+    AccountIDGenerator account_id_generator;
+
+    // while the Account ID generated is invalid
+    uint32_t account_id = account_id_generator.generate_new_id();
+    while (!account_id_validator.is_account_id_valid(account_id, accounts)) {
+        account_id = account_id_generator.generate_new_id();
+    }
+    return account_id;
 }
